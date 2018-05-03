b93c01df414b070cdc5a80280e4e18e2fc6c6cee
Camden C <camdenc.cornwall@gmail.com>
2018-05-03 16:35:43 -0600

    adding index and home page

---
+++ web/home.php
@@ -0,0 +1,40 @@
+       
+<?php
+/**
+* MAIN HOME INDEX
+*/
+ $action = filter_input(INPUT_POST, 'action');
+if ($action == NULL){
+ $action = filter_input(INPUT_GET, 'action');
+if($action == NULL){
+ $action = 'home';
+}
+if($action == NULL){
+ $action = 'Assignments';
+}
+
+
+}
+
+switch ($action) {
+    
+ case 'home':
+  include ($_SERVER['DOCUMENT_ROOT'].'/web/home.php');
+break;
+ 
+  case 'Assignments':
+     include ($_SERVER['DOCUMENT_ROOT'].'/DateGame/views/D8DTgameList/index.php');
+     break;
+ 
+}
+
+?>
+        
+
+
+        
+        <div></div>
+    </body>
+</html>
+
+
