cf2c9f3fccf0703b3395c89ea72dbdd4112e459e
Camden C <camdenc.cornwall@gmail.com>
2018-05-18 11:08:22 -0600

    trying to connect to the database

--- web/connecttoSQL.php
+++ web/connecttoSQL.php
@@ -8,7 +8,7 @@ $dsn = "mysql:host=$server;dbname=$database";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 try {
-    $conn_string = "host=ec2-107-21-103-146.compute-1.amazonaws.com port=5432 dbname=dfu4ugpnl0g4u6 user=wxpfcgdwrtkeuu password=5d735d5b276a613f73e567f2e5eebc8e6c5d28151e1097cb243723957bd61442";
+    $conn_string = "host=$server dbname=$database user=$user password=$password";
     $dbconn4 = pg_connect($conn_string);
     //$acmeLink = new PDO($dsn, $user, $password, $options);
     echo 'Connection worked';
