097e6ba9ac168307d2482aef5e5e30862421871f
Camden C <camdenc.cornwall@gmail.com>
2018-05-18 10:25:26 -0600

    trying to connect to the database

---
+++ web/connecttoSQL.php
@@ -0,0 +1,18 @@
+<?php
+function connect(){
+$server = "ec2-107-21-103-146.compute-1.amazonaws.com";
+$database = "dfu4ugpnl0g4u6";
+$user = "wxpfcgdwrtkeuu";
+$password = "5d735d5b276a613f73e567f2e5eebc8e6c5d28151e1097cb243723957bd61442";
+$dsn = "host=$server;dbname=$database";
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+try {
+    $acmeLink = new PDO($dsn, $user, $password, $options);
+//    echo 'Connection worked';
+    return $acmeLink;
+} catch (PDOException $ex) {
+    header('location: \acme\acmeError.php'); 
+//    header('location: \acme\index.php'); 
+}
+}
