e3979f7beb15107bff9a7ce7595c45d5d62646cb
Camden C <camdenc.cornwall@gmail.com>
2018-05-18 10:43:05 -0600

    trying to connect to the database

--- web/connecttoSQL.php
+++ web/connecttoSQL.php
@@ -4,7 +4,7 @@ $server = "ec2-107-21-103-146.compute-1.amazonaws.com";
 $database = "dfu4ugpnl0g4u6";
 $user = "wxpfcgdwrtkeuu";
 $password = "5d735d5b276a613f73e567f2e5eebc8e6c5d28151e1097cb243723957bd61442";
-$dsn = "host=$server;dbname=$database";
+$dsn = "mysql:host=$server;dbname=$database";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 try {
